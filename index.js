cf66c1c28b746cb39bf9f8c3b24d9e5c1dea46b4
Neeraj Dalal <nd941z@gmail.com>
2021-05-04 13:34:50 +0530

    Add config files.

--- index.js
+++ index.js
@@ -110,3 +110,10 @@ cron.schedule('*/15 * * * *', () => {
 		na53Nq('USDT')
 	}, 7000)
 })
+
+cron.schedule('0 * /4 * * *', () => {
+	axios.post('https://api.telegram.org/bot1756916114:AAHutD0mn_OWLFyX6J43deLG0RY-hNLMjL8/sendMessage', {
+		chat_id: '@na53Nq',
+		text: 'All systems active',
+	})
+})
